ebe727ad721255694ed4fcca75fac77c65d9e60a
K.Lab <Kevin-Moritz-2001@outlook.de>
2023-06-30 15:47:51 +0200

    Calc possible move for knight

--- src/board.py
+++ src/board.py
@@ -1,6 +1,7 @@
 from const import *
 from square import Square
 from piece import *
+from move import Move
 
 
 class Board:
@@ -11,12 +12,67 @@ class Board:
         self._add_pieces("white")
         self._add_pieces("black")
 
-    def calc_moves(self, piece, row, cal):
+    def calc_moves(self, piece, row, col):
         """
         calculate all the possible (valid) moves of an specific piece on a specific position
         """
 
-        
+        def pawn_moves():
+            steps = 1 if piece.moved else 2
+
+            start = row + piece.dir
+            end = row + (piece.dir * (1 + steps))
+
+            for move_row in range(start, end, piece.dir):
+                pass
+
+            # FIXME: https://www.youtube.com/watch?v=OpL0Gcfn4B4  2:16:30
+
+        def knight_moves():
+            # 8 possible moves
+            possible_moves = [
+                (row - 2, col + 1),
+                (row - 1, col + 2),
+                (row + 1, col + 2),
+                (row + 2, col + 1),
+                (row + 2, col - 1),
+                (row + 1, col - 2),
+                (row - 1, col - 2),
+                (row - 2, col - 1),
+            ]
+
+            for possible_move in possible_moves:
+                possible_move_row, possible_move_col = possible_move
+
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row][
+                        possible_move_col
+                    ].isempty_or_rival(piece.color):
+                        # create squares of the new move
+                        initial = Square(row, col)
+                        final = Square(possible_move_row, possible_move_col)
+                        # create new move
+                        move = Move(initial, final)
+                        # append new valid move
+                        piece.add_move(move)
+
+        if isinstance(piece, Pawn):
+            pawn_moves()
+
+        elif isinstance(piece, Knight):
+            knight_moves()
+
+        elif isinstance(piece, Bishop):
+            pass
+
+        elif isinstance(piece, Rook):
+            pass
+
+        elif isinstance(piece, Queen):
+            pass
+
+        elif isinstance(piece, King):
+            pass
 
     def _create(self):
         for row in range(ROWS):
--- src/main.py
+++ src/main.py
@@ -23,7 +23,10 @@ class Main:
 
         # Game loop
         while True:
+
+            # show methods
             game.show_bg(screen)
+            game.show_moves(screen)
             game.show_pieces(screen)
 
             if dragger.dragging:
@@ -41,14 +44,21 @@ class Main:
                     # if clicked square has a piece ?
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col].piece
+                        board.calc_moves(piece, clicked_row, clicked_col)
                         dragger.save_initial(event.pos)
                         dragger.drag_piece(piece)
+                        # show methods
+                        game.show_bg(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
 
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
+                        # show methods
                         game.show_bg(screen)
+                        game.show_moves(screen)
                         game.show_pieces(screen)
                         dragger.update_blit(screen)
 
@@ -68,4 +78,3 @@ main = Main()
 main.mainloop()
 
 
-# min 31.37
